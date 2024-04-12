634b15194384d3bee975e276d5499857d8a19c9e
sigmotoa <si.galvismotoa@gmail.com>
2024-04-12 16:45:47 -0500

    Heroes OOP

---
+++ Super Hero/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var profesorX = new Heroe();
+profesorX.Id = 1;
+profesorX.Nombre = "Profesor X";
+profesorX.Identidad = "Charles Xavier";
+profesorX.Ciudad = "UK";
+profesorX.Poderes = new[] { "Telepatia", "Telekinesis", "Liderazgo", "Adquisitivo" };
+
+Console.WriteLine(profesorX.Nombre);
+class Heroe
+{
+   public int Id;
+   public string Nombre;
+   public string Identidad;
+   public string Ciudad;
+   public string[] Poderes;
+
+}
