a22a5552377870b724e638367a808812723f03c0
sigmotoa <si.galvismotoa@gmail.com>
2024-04-12 16:59:35 -0500

    Poderes en clase

--- Super Hero/Program.cs
+++ Super Hero/Program.cs
@@ -7,15 +7,35 @@ profesorX.Id = 1;
 profesorX.Nombre = "Profesor X";
 profesorX.Identidad = "Charles Xavier";
 profesorX.Ciudad = "UK";
-profesorX.Poderes = new[] { "Telepatia", "Telekinesis", "Liderazgo", "Adquisitivo" };
+profesorX.Volar = false;
+
+var imbecibilidad = new Poder();
+imbecibilidad.Nombre = "Imbecibilidad";
+imbecibilidad.Descripcion = "Firmar por tu compañere que no vine";
+imbecibilidad.Nivel = "Nivel 1";
+
+var alientoFuego = new Poder();
+alientoFuego.Nombre = "Aliento de Fuego";
+alientoFuego.Descripcion = "Lanzar fuego por la boca";
+alientoFuego.Nivel = "Semaforo";
+profesorX.Poderes = new[] {alientoFuego, imbecibilidad};
 
 Console.WriteLine(profesorX.Nombre);
+Console.WriteLine(profesorX.Poderes[0].Nombre);
 class Heroe
 {
    public int Id;
    public string Nombre;
    public string Identidad;
    public string Ciudad;
-   public string[] Poderes;
+   public Poder[] Poderes;
+   public bool Volar;
 
+}
+
+class Poder
+{
+   public string Nombre;
+   public string Descripcion;
+   public string Nivel;
 }
